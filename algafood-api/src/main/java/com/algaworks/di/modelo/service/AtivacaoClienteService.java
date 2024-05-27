3188d934d7560df7d3bb0571b1c87bb76bda1af4
Vagner Braga <haigorne@gmail.com>
2024-05-27 10:38:42 -0300

    [ESR 02x14/15] Injeção de dependências

--- algafood-api/src/main/java/com/algaworks/di/modelo/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/di/modelo/service/AtivacaoClienteService.java
@@ -1,18 +1,37 @@
 package com.algaworks.di.modelo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.di.modelo.Cliente;
 import com.algaworks.di.modelo.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 	
-	private Notificador notificador;
+	/*
+	 * Exemplos de pontos de injeção de dependência:
+	 *  via construtor - 1
+	 *  via setter - 2 
+	 *  via variável de instância - 3
+	 */
 	
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-	}
+	@Autowired
+	private Notificador notificador; // 3 - ' add @Autowired'
+	
+	/* (1)
+	 * @Autowired public AtivacaoClienteService(Notificador notificador) {
+	 * this.notificador = notificador; }
+	 */
 
 	public void ativar (Cliente cliente) {
 		cliente.ativar(true);
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
+	
+	//(2)
+	/*
+	 * @Autowired public void setNotificador(Notificador notificador) {
+	 * this.notificador = notificador; } }
+	 */
 }
