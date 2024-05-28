21ec74ea1a32e83130b7f34453b4a6cf1e369bfc
Vagner Braga <haigorne@gmail.com>
2024-05-28 11:01:22 -0300

    [ESR 02x16] Dependência opcional dentro de um Bean

--- algafood-api/src/main/java/com/algaworks/di/modelo/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/di/modelo/service/AtivacaoClienteService.java
@@ -8,30 +8,18 @@ import com.algaworks.di.modelo.notificacao.Notificador;
 
 @Component
 public class AtivacaoClienteService {
-	
-	/*
-	 * Exemplos de pontos de injeção de dependência:
-	 *  via construtor - 1
-	 *  via setter - 2 
-	 *  via variável de instância - 3
-	 */
-	
-	@Autowired
-	private Notificador notificador; // 3 - ' add @Autowired'
-	
-	/* (1)
-	 * @Autowired public AtivacaoClienteService(Notificador notificador) {
-	 * this.notificador = notificador; }
-	 */
 
+	@Autowired(required = false)//Dependência opcional dentro de um Bean
+	private Notificador notificador;
+	
 	public void ativar (Cliente cliente) {
 		cliente.ativar(true);
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		
+		if(notificador != null) {
+			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		}else {
+			System.out.println("Não existe notificador. Mas, o cliente foi ativado.");
+		}
 	}
 	
-	//(2)
-	/*
-	 * @Autowired public void setNotificador(Notificador notificador) {
-	 * this.notificador = notificador; } }
-	 */
 }
