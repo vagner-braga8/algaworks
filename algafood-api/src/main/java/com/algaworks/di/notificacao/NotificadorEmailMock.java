d7ca27e51a1d2cced0ebbf06e94ce1e80736cd4c
Vagner Braga <haigorne@gmail.com>
2024-05-31 10:53:16 -0300

    [ESR 02x27] Alterando a config do projeto de acordo com o ambiente

--- algafood-api/src/main/java/com/algaworks/di/notificacao/NotificadorEmailMock.java
+++ algafood-api/src/main/java/com/algaworks/di/notificacao/NotificadorEmailMock.java
@@ -1,5 +1,6 @@
 package com.algaworks.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +10,16 @@ import com.algaworks.di.modelo.Cliente;
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmailMock implements Notificador {
+	
+	@Autowired
+	private NotificadorProperties notificadorProperties;
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		
+		System.out.println("Host-dev: " + notificadorProperties.getHostServidor());
+		System.out.println("Porta-dev: " + notificadorProperties.getPortaServidor());
+		
 		System.out.printf("MOCK: Notificando seria enviada para %s através do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
