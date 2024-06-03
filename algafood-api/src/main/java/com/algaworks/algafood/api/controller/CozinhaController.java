dbfae295a064b180e9129691ff05819ada87a31b
Vagner Braga <haigorne@gmail.com>
2024-06-03 16:44:59 -0300

    [ESR 04x16] Customizando a representação em XML com Wrapper e anotações

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,16 +3,18 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -24,6 +26,12 @@ public class CozinhaController {
 	@GetMapping
 	public List<Cozinha> listar(){
 		return cozinhaRepository.buscarTodas();
+	}
+	
+	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml(){
+		return new CozinhasXmlWrapper(cozinhaRepository.buscarTodas());
 	}
 	
 	//ResponseEntity : Permite 'customizar' a resposta HTTP
