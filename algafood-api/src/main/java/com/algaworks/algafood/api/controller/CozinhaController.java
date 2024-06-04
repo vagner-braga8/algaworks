0b0b0acb83abc0b54bd39aadcd67baf05480d879
Vagner Braga <haigorne@gmail.com>
2024-06-04 12:50:55 -0300

    [ESR 04x29] Desafio modelando e implementando a consulta de recursos

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -37,11 +35,6 @@ public class CozinhaController {
 	@GetMapping
 	public List<Cozinha> listar() {
 		return cozinhaRepository.buscarTodas();
-	}
-
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml() {
-		return new CozinhasXmlWrapper(cozinhaRepository.buscarTodas());
 	}
 
 	@GetMapping("/{id}")
@@ -64,7 +57,7 @@ public class CozinhaController {
 		Cozinha cozinhaAtual = cozinhaRepository.buscarPorId(id);
 		if (cozinhaAtual != null) {
 			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
-			cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+			cozinhaAtual = cadastroCozinhaService.salvar(cozinhaAtual);
 			return ResponseEntity.ok(cozinhaAtual);
 		}
 		return ResponseEntity.notFound().build();
---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.service.ConsultaRestauranteService;
+
+@RestController
+@RequestMapping("/restaurantes")
+public class RestauranteController {
+	
+	@Autowired
+	private ConsultaRestauranteService consultaRestauranteService;
+
+	@GetMapping
+	public List<Restaurante> buscarTodos() {
+		return consultaRestauranteService.buscarTodos();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Restaurante> buscarPorId(@PathVariable Long id) {
+		Restaurante restaurante = consultaRestauranteService.buscarPorId(id);
+		if (restaurante != null) {
+			return ResponseEntity.ok(restaurante);
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+}
