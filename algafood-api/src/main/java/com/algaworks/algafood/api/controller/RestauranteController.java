8edd7faacfaa6b9ee7a4dc83dbcb6c7da6175a28
Vagner Braga <haigorne@gmail.com>
2024-06-07 11:50:25 -0300

    [ESR 04x33/34] Atualização parcial com a API de Reflections

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,6 +23,7 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -62,6 +67,41 @@ public class RestauranteController {
 			return ResponseEntity.ok(restauranteAtual);
 		}
 		return ResponseEntity.notFound().build();
+	}
+
+	@PatchMapping("/{id}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long id, @RequestBody Map<String, Object> campos) {
+		Restaurante restauranteAtual = cadastroRestauranteService.buscarPorId(id);
+
+		if (restauranteAtual == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		merge(campos, restauranteAtual);
+
+		return atualizar(id, restauranteAtual);
+	}
+
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+		// Cria uma instância de ObjectMapper. Que é usada para manipular e converter objetos Java em JSON e vice-versa.
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		// Converte o mapa dadosOrigem em um objeto do tipo Restaurante.
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+
+			// Usa a reflexão para encontrar o campo (field) na classe Restaurante que corresponde ao nome da propriedade.
+			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+
+			// Torna o campo acessível, permitindo que ele possa ser modificado mesmo que seja privado.
+			field.setAccessible(true);
+
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			
+			// Usa a reflexão para definir o valor do campo encontrado (field) no objeto
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+		});
 	}
 
 }
