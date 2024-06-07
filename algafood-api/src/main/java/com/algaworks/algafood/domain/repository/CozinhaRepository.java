ece9e2c2ae388659f30541a2c59a9e23c783cda2
Vagner Braga <haigorne@gmail.com>
2024-06-07 12:12:55 -0300

    [ESR 05x01] Implementando consultas JPQL em repositórios

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -7,6 +7,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 public interface CozinhaRepository {
 	
 	List<Cozinha> buscarTodas();
+	List<Cozinha> ConsultarPorNome(String nome);
 	Cozinha buscarPorId(Long id);
 	Cozinha salvar(Cozinha cozinha);
 	void remover(Long id);
--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -12,7 +13,7 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository{
 
 	@PersistenceContext
@@ -42,6 +43,13 @@ public class CozinhaRepositoryImpl implements CozinhaRepository{
 			throw new EmptyResultDataAccessException(1);
 		}
 		manager.remove(cozinha);
+	}
+
+	@Override
+	public List<Cozinha> ConsultarPorNome(String nome) {
+		return manager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+				.setParameter("nome", "%" + nome + "%")
+				.getResultList();
 	}
 	
 }
