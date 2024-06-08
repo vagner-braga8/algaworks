df76fe76ac941a9f86762995e0d6e78d67e1fe00
Vagner Braga <haigorne@gmail.com>
2024-06-08 13:43:55 -0300

    [ESR 05x06] Criando consultas com query methods

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
+	
+	List<Cozinha> findByNome(String nome);
 	
 }
