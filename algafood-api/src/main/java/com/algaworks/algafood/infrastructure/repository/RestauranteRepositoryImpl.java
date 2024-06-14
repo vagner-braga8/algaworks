3bc08e483fb161ec86ecd7e5497cad026c0e0b8b
Vagner <haigorne@gmail.com>
2024-06-14 10:34:20 -0300

    [ESR 05x15] Tornando a consulta com Criteria API com filtros dinâmicos

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,16 +34,21 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		
 		Root<Restaurante> root = criteria.from(Restaurante.class);
 		
-		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%" );
+		var predicates = new ArrayList<Predicate>();
 		
-		//greaterThanOrEqualTo '>='
-		Predicate taxaFreteInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		if(StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%" ));
+		}
 		
-		//lessThanOrEqualTo  '<='
-		Predicate taxaFreteFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		if(taxaFreteInicial != null) { 
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		}
 		
-		criteria.where(nomePredicate, taxaFreteInicialPredicate, taxaFreteFinalPredicate );
+		if(taxaFreteFinal != null) { 
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
 		
+		criteria.where(predicates.toArray(new Predicate[0]));
 		
 		TypedQuery<Restaurante> query = entityManager.createQuery(criteria);
 		return query.getResultList();
