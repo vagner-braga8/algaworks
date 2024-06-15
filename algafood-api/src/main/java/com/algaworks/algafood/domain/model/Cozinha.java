ab8d86011382d5bd821e94ab2cdd90e7c19678d6
Vagner <haigorne@gmail.com>
2024-06-15 13:57:44 -0300

    [ESR 06x01] Mapeando relacionamento bidirecional com @OneToMany

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,10 +1,16 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -20,5 +26,15 @@ public class Cozinha {
 	
 	@Column(nullable = false)
 	private String nome;
+
+	/*
+	 * JsonIgnore : Não serialize a entidade 'Restaurante' quando for serializar a entidade 'Cozinha' 
+	 * OneToMany : Uma cozinha para muitos restaurantes,
+	 * mappedBy: "cozinha" : Que foi mapeado por cozinha,
+	 * Onde? : List<Restaurante> , em Restaurante.
+	 */
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 	
 }
