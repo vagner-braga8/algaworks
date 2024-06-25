01481168fe1b45fc767faf59f28c317cd6d8d65a
Vagner <haigorne@gmail.com>
2024-06-25 13:56:27 -0300

    [ESR 07x06] Adicionando o Flyway no projeto e criando a primeira migração

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -27,6 +27,9 @@ public class Cozinha {
 	@Column(nullable = false)
 	private String nome;
 	
+	@Column(nullable = false)
+	private String descricao;
+	
 	@JsonIgnore
 	@OneToMany(mappedBy = "cozinha")
 	private List<Restaurante> restaurantes = new ArrayList<>();
